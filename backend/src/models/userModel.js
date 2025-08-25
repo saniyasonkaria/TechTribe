1bfa9967a483557d35d04e3d882fe36660627986
Saniya Sonkaria <saniya.sonkaria@waiin.com>
2025-08-25 13:10:11 +0530

    Never trust on res.body . add validation and sanitize it

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -1,44 +1,59 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minlength: 3,
-    maxlength: 30,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    min: 12,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if (!["male", "female", "other"].includes(value.toLowerCase())) {
-       throw new Error ("Gender could be male, female or other");
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 30,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 12,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value.toLowerCase())) {
+          throw new Error("Gender could be male, female or other");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-File.png",
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Photo URL is invalid");
+        }
+      },
     },
   },
-  photoUrl: {
-    type: String,
-    default: "https://www.pngall.com/wp-content/uploads/5/Profile-PNG-File.png",
-  },
-},{ timestamps: true });
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
