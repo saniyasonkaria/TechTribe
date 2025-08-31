533b88145862719b59ae2d4a8f8763c51f7d4720
Saniya Sonkaria <saniya.sonkaria@waiin.com>
2025-08-31 15:18:50 +0530

    Added /profile/password api to update the password for the user

--- backend/src/routes/profile.js
+++ backend/src/routes/profile.js
@@ -2,13 +2,14 @@ const express = require("express");
 const profileRoute = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validateProfileEditData } = require("../utils/validate");
+const bcrypt = require("bcrypt");
 
 profileRoute.get("/profile/view", userAuth, async (req, res) => {
   try {
     //fetch user profile from database
     res.send("user profile data" + req.user);
   } catch (err) {
-    console.log(err);
+    console.error(err);
     res.status(400).send(err.message);
   }
 });
@@ -22,14 +23,33 @@ profileRoute.patch("/profile/edit", userAuth, async (req, res) => {
     const editFields = req.body;
     console.log(loggedInUser);
     Object.keys(req.body).forEach(
-      (key) => loggedInUser[key] = editFields[key]
+      (key) => (loggedInUser[key] = editFields[key])
     );
 
     await loggedInUser.save();
     console.log(loggedInUser);
     res.send("Profile Edited Successfully!!");
   } catch (err) {
-    console.log(err);
+    console.error(err);
+    res.status(400).send(err.message);
+  }
+});
+
+profileRoute.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { currPassword, newPassword } = req.body;
+    const loggedInUser = req.user;
+
+    const match = await bcrypt.compare(currPassword, loggedInUser.password);
+    if (!match) {
+      throw new Error("Invalid Credential!!");
+    }
+    const hashedNewPassword = bcrypt.hashSync(newPassword, 10);
+    loggedInUser.password = hashedNewPassword;
+    loggedInUser.save();
+    res.send(`${loggedInUser.firstName}'s password is Updated!!`);
+  } catch (err) {
+    console.error(err);
     res.status(400).send(err.message);
   }
 });
