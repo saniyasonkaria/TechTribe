0ed3794a5484af9b9ca4dfe5d835191105d667e8
Saniya Sonkaria <saniya.sonkaria@waiin.com>
2025-09-13 01:11:19 +0530

    Added tech tribe logo and favicon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,28 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+import { removeUser } from "../utils/userSlice";
 
 const Navbar = () => {
   const userData = useSelector((store) => store.user);
-  console.log("userData", userData);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await axios.post(BASE_URL + "/logout",{}, { withCredentials: true });
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
-    <div className="navbar ">
-      <div className="flex-1">
-        <a className="btn bbg-base-200tn-ghost text-xl">daisyUI</a>
+    <div className="navbar bg-base-200 border ">
+      <div className="flex-1 align-baseline ">
+        <Link to="/" className="btn btn-ghost text-xl">
+          <img src="./techTribe.jpg" alt="logo" width={"60px"} />
+        </Link>
       </div>
       <div className="flex-none gap-2">
         {userData && (
@@ -27,16 +43,16 @@ const Navbar = () => {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
                 </li>
                 <li>
-                  <a>Logout</a>
+                  <a onClick={handleLogout}>Logout</a>
                 </li>
               </ul>
             </div>
